47c8dc19b056e9f476d87cbc7bda560e4edc8a07
Lari07 <Larissa_Fahrni@sluz.ch>
2024-09-19 11:04:49 +0200

    linter test

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+
+
+export default [
+  { rules: { 
+    "no-unused-vars": "error",
+        "prefer-const": "error",
+        "eqeqeq": ["error", "always"]
+}, 
+},
+];
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import chalk from "chalk";
 
 console.log(chalk.blue("It works"));
+
+let myUnusedVariable = 42; 
+if (myUnusedVariable == 42) { 
+    console.log("This should trigger the 'eqeqeq' rule");
+}
